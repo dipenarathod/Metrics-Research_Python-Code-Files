b9c3bcbf2e29a88788326de6b1920a90eaac795c
dipenarathod <dipenArathod@gmail.com>
2025-06-01 12:42:01 +0530

    Update Square.py

--- Square.py
+++ Square.py
@@ -3,5 +3,12 @@ class Square(Rectangle):
     """Square shape class."""
     
     def __init__(self, side):
+        for i in range(10):
+            side+=2
         super().__init__(side, side)
-        self.name = "Square"
+        if(side <= 0):
+            self.name = "Invalid Square"
+        elif(side >= 100):
+            self.name = "XL Square"
+        else:
+            self.name = "Square"
